51de57205a88ea67fe021caafe02dbef35ca12dd
bc <bc@bcnoMacBook-Pro.local>
2019-09-12 17:30:32 +0900

    update contentbuilder

--- wolf-strategy/aiwolfpy/contentbuilder.py
+++ wolf-strategy/aiwolfpy/contentbuilder.py
@@ -1,47 +1,80 @@
 # 2.1
-def estimate(target, role):
+def ESTIMATE(target, role):
     return 'ESTIMATE Agent[' + "{0:02d}".format(target) + '] ' + role
 
-def comingout(target, role):
+
+def COMINGOUT(target, role):
     return 'COMINGOUT Agent[' + "{0:02d}".format(target) + '] ' + role
 
 # 2.2
-def divine(target):
+
+
+def DIVINE(target):
     return 'DIVINE Agent[' + "{0:02d}".format(target) + ']'
-    
-def guard(target):
+
+
+def GUARD(target):
     return 'GUARD Agent[' + "{0:02d}".format(target) + ']'
-    
-def vote(target):
+
+
+def VOTE(target):
     return 'VOTE Agent[' + "{0:02d}".format(target) + ']'
 
-def attack(target):
+
+def ATTACK(target):
     return 'ATTACK Agent[' + "{0:02d}".format(target) + ']'
 
 # 2.3
-def divined(target, species):
+
+
+def DIVINED(target, species):
     return 'DIVINED Agent[' + "{0:02d}".format(target) + '] ' + species
 
-def identified(target, species):
+
+def IDENTIFIED(target, species):
     return 'IDENTIFIED Agent[' + "{0:02d}".format(target) + '] ' + species
 
-def guarded(target):
+
+def GUARDED(target):
     return 'GUARDED Agent[' + "{0:02d}".format(target) + ']'
 
+
+def VOTED(target):
+    return 'VOTED Agent [' + "{0:02d}".format(target) + ']'
+
+
+def ATTACKED(target):
+    return 'ATTACKED Agent [' + "{0:02d}".format(target) + ']'
+
+
+"""
 # 2.4
 def agree(talktype, day, id):
     return 'AGREE '+ talktype + ' day' + str(day) + ' ID:' + str(id)
 
 def disagree(talktype, day, id):
     return 'DISAGREE '+ talktype + ' day' + str(day) + ' ID:' + str(id)
+"""
 
+
+def AGREE(talknumber):
+    return 'AGREE' + str(talknumber)
+
+
+def DISAGREE(talknumber):
+    return 'DISAGREE' + str(talknumber)
 # 2.5
+
+
 def skip():
     return 'Skip'
+
 
 def over():
     return 'Over'
 
 # 3
+
+
 def request(text):
     return 'REQUEST(' + text + ''
